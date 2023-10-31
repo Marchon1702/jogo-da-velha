9345aa3a4b0f42d5396624f522f94193c9793030
Matheus Marchon <matheus.s.marchon@hotmail.com>
2023-10-31 02:30:28 -0300

    reajuste de lógica

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var vez = 'O'
+var veznome = ''
 var a1 = document.getElementById('a1')
 var a2 = document.getElementById('a2')
 var a3 = document.getElementById('a3')
@@ -9,6 +10,30 @@ var c1 = document.getElementById('c1')
 var c2 = document.getElementById('c2')
 var c3 = document.getElementById('c3')
 var resul = document.getElementById('res')
+var p1 = document.getElementById('p1')
+var p2 = document.getElementById('p2')
+var joga1 = document.getElementById('joga1')
+var joga2 = document.getElementById('joga2')
+var placar1 = document.getElementById('placar1')
+var placar2 = document.getElementById('placar2')
+var empate = false
+
+
+function enviNome() {
+    if (p1.value == '' || p2.value == '') {
+        joga1.innerHTML = 'Player 1'
+        joga2.innerHTML = 'Player 2'
+        resul.style.display = 'none'
+        tabuleiro.style.display = 'block'
+        restart.style.display = 'block'
+    } else {
+        joga1.innerHTML = p1.value
+        joga2.innerHTML = p2.value
+        resul.style.display = 'none'
+        tabuleiro.style.display = 'block'
+        restart.style.display = 'block'
+    }
+}
 
 function vezdoplayer(v) {
     if(vez == 'O') {
@@ -31,119 +56,98 @@ function vezdoplayer(v) {
 }
 
 function fimdejogo() {
-if(a1.innerHTML == 'X' 
-    && b2.innerHTML == 'X' 
-    && c3.innerHTML == 'X') {   
-        tabuleiro.style.display = 'none'
-        resul.style.display = 'grid'
-        resul.innerHTML = 'Jogador X venceu! 🥇'
-    } else if (a3.innerHTML == 'X' &&
-               b2.innerHTML == 'X' &&
-               c1.innerHTML == 'X') {
-                 tabuleiro.style.display = 'none'
-                 resul.style.display = 'grid'
-                 resul.innerHTML = 'Jogador X venceu! 🥇'
-               } else if (a1.innerHTML == 'X' &&
-                          b1.innerHTML == 'X' &&
-                          c1.innerHTML == 'X') {
-                            tabuleiro.style.display = 'none'
-                            resul.style.display = 'grid'
-                            resul.innerHTML = 'Jogador X venceu! 🥇'
-                          } else if (a2.innerHTML == 'X' &&
-                                     b2.innerHTML == 'X' &&
-                                     c2.innerHTML == 'X') {
-                                        tabuleiro.style.display = 'none'
-                                        resul.style.display = 'grid'
-                                        resul.innerHTML = 'Jogador X venceu! 🥇'
-                            } else if (a3.innerHTML == 'X' &&
-                                     b3.innerHTML == 'X' &&
-                                     c3.innerHTML == 'X') {
-                                        tabuleiro.style.display = 'none'
-                                        resul.style.display = 'grid'
-                                        resul.innerHTML = 'Jogador X venceu! 🥇'
-                                } else if (a1.innerHTML == 'X' &&
-                                           a2.innerHTML == 'X' &&
-                                           a3.innerHTML == 'X') {
-                                            tabuleiro.style.display = 'none'
-                                            resul.style.display = 'grid'
-                                            resul.innerHTML = 'Jogador X venceu! 🥇'
-                                    } else if (a1.innerHTML == 'X' &&
-                                               a2.innerHTML == 'X' &&
-                                               a3.innerHTML == 'X') {
-                                                tabuleiro.style.display = 'none'
-                                                resul.style.display = 'grid'
-                                                resul.innerHTML = 'Jogador X venceu! 🥇'
-                                        } else if (b1.innerHTML == 'X' &&
-                                                b2.innerHTML == 'X' &&
-                                                b3.innerHTML == 'X') {
-                                                 tabuleiro.style.display = 'none'
-                                                 resul.style.display = 'grid'
-                                                 resul.innerHTML = 'Jogador X venceu! 🥇'
-                                            } else if (c1.innerHTML == 'X' &&
-                                                      c2.innerHTML == 'X' &&
-                                                      c3.innerHTML == 'X') {
-                                                        tabuleiro.style.display = 'none'
-                                                        resul.style.display = 'grid'
-                                                        resul.innerHTML = 'Jogador X venceu! 🥇'
-                                                        } 
-if(a1.innerHTML == 'O' 
-&& b2.innerHTML == 'O' 
-&& c3.innerHTML == 'O') {   
-    tabuleiro.style.display = 'none'
-    resul.style.display = 'grid'
-    resul.innerHTML = 'Jogador O venceu! 🥇'
-    } else if (a3.innerHTML == 'O' &&
-                b2.innerHTML == 'O' &&
-                c1.innerHTML == 'O') {
-                    tabuleiro.style.display = 'none'
-                    resul.style.display = 'grid'
-                    resul.innerHTML = 'Jogador O venceu! 🥇'
-                    } else if (a1.innerHTML == 'O' &&
-                              b1.innerHTML == 'O' &&
-                              c1.innerHTML == 'O') {
-                                    tabuleiro.style.display = 'none'
-                                    resul.style.display = 'grid'
-                                    resul.innerHTML = 'Jogador O venceu! 🥇'
-                                    } else if (a2.innerHTML == 'O' &&
-                                               b2.innerHTML == 'O' &&
-                                               c2.innerHTML == 'O') {
-                                               tabuleiro.style.display = 'none'
-                                                resul.style.display = 'grid'
-                                                resul.innerHTML = 'Jogador O venceu! 🥇'
-                                                } else if (a3.innerHTML == 'O' &&
-                                                            b3.innerHTML == 'O' &&
-                                                            c3.innerHTML == 'O') {
-                                                                tabuleiro.style.display = 'none'
-                                                                resul.style.display = 'grid'
-                                                                resul.innerHTML = 'Jogador O venceu! 🥇'
-                                                                } else if (a1.innerHTML == 'O' &&
-                                                                            a2.innerHTML == 'O' &&
-                                                                            a3.innerHTML == 'O') {
-                                                                                tabuleiro.style.display = 'none'
-                                                                                resul.style.display = 'grid'
-                                                                                resul.innerHTML = 'Jogador O venceu! 🥇'
-                                                                                } else if (a1.innerHTML == 'O' &&
-                                                                                            a2.innerHTML == 'O' &&
-                                                                                            a3.innerHTML == 'O') {
-                                                                                                tabuleiro.style.display = 'none'
-                                                                                                resul.style.display = 'grid'
-                                                                                                resul.innerHTML = 'Jogador O venceu! 🥇'
-                                                                                            } else if (b1.innerHTML ==      'O' &&
-                                                                                                    b2.innerHTML == 'O' &&
-                                                                                                    b3.innerHTML == 'O') {
-                                                                                                     tabuleiro.style.display = 'none'
-                                                                                                     resul.style.display = 'grid'
-                                                                                                     resul.innerHTML = 'Jogador O venceu! 🥇'
-                                                                                                } else if (c1.innerHTML == 'O' &&
-                                                                                                          c2.innerHTML == 'O' &&
-                                                                                                          c3.innerHTML == 'O') {
-                                                                                                            tabuleiro.style.display = 'none'
-                                                                                                            resul.style.display = 'grid'
-                                                                                                            resul.innerHTML = 'Jogador O venceu! 🥇'
-                                                                                                            } 
+    // Verificar linhas
+    if (
+        (a1.innerHTML !== '' && a1.innerHTML === a2.innerHTML && a1.innerHTML === a3.innerHTML) ||
+        (b1.innerHTML !== '' && b1.innerHTML === b2.innerHTML && b1.innerHTML === b3.innerHTML) ||
+        (c1.innerHTML !== '' && c1.innerHTML === c2.innerHTML && c1.innerHTML === c3.innerHTML)
+    ) {
+        
+        if(vez == 'X' && p1.value != '') {
+            veznome = p1.value
+        } else if (vez == 'X' && p1.value == '') {
+            veznome = 'Player 1'
+        }
 
+        if(vez == 'O' && p2.value != '') {
+            veznome = p2.value
+        } else if (vez == 'O' && p2.value == '') {
+            veznome = 'Player 2'
+        }
+
+        resul.innerHTML = `${veznome} Venceu! 🏅`
+        resul.style.display = 'grid'
+        tabuleiro.style.display = 'none'
+        atualizarPlacar(vez)
+        return
+    }
+
+    // Verificar colunas
+    if (
+        (a1.innerHTML !== '' && a1.innerHTML === b1.innerHTML && a1.innerHTML === c1.innerHTML) ||
+        (a2.innerHTML !== '' && a2.innerHTML === b2.innerHTML && a2.innerHTML === c2.innerHTML) ||
+        (a3.innerHTML !== '' && a3.innerHTML === b3.innerHTML && a3.innerHTML === c3.innerHTML)
+    ) {
+        if(vez == 'X' && p1.value != '') {
+            veznome = p1.value
+        } else if (vez == 'X' && p1.value == '') {
+            veznome = 'Player 1'
+        }
+
+        if(vez == 'O' && p2.value != '') {
+            veznome = p2.value
+        } else if (vez == 'O' && p2.value == '') {
+            veznome = 'Player 2'
+        }
+        resul.innerHTML = `${veznome} Venceu! 🏅`
+        resul.style.display = 'grid'
+        tabuleiro.style.display = 'none'
+        atualizarPlacar(vez)
+    }
+
+    // Verificar diagonais
+    if (
+        (a1.innerHTML !== '' && a1.innerHTML === b2.innerHTML && a1.innerHTML === c3.innerHTML) ||
+        (a3.innerHTML !== '' && a3.innerHTML === b2.innerHTML && a3.innerHTML === c1.innerHTML)
+    ) {
+        if(vez == 'X' && p1.value != '') {
+            veznome = p1.value
+        } else if (vez == 'X' && p1.value == '') {
+            veznome = 'Player 1'
+        }
+
+        if(vez == 'O' && p2.value != '') {
+            veznome = p2.value
+        } else if (vez == 'O' && p2.value == '') {
+            veznome = 'Player 2'
+        }
+        resul.innerHTML = `${veznome} Venceu! 🏅`
+        resul.style.display = 'grid'
+        tabuleiro.style.display = 'none'
+        atualizarPlacar(vez)
+    }
+
+    // Verificar empate
+    if (
+        a1.innerHTML !== '' && a2.innerHTML !== '' && a3.innerHTML !== '' &&
+        b1.innerHTML !== '' && b2.innerHTML !== '' && b3.innerHTML !== '' &&
+        c1.innerHTML !== '' && c2.innerHTML !== '' && c3.innerHTML !== ''
+    ) {
+        resul.innerHTML = 'Velha 🤝'
+        resul.style.display = 'grid'
+        tabuleiro.style.display = 'none'
+        empate = true
+    }
 }
 
+function atualizarPlacar(vencedor) {
+    if (vencedor === 'X' && empate == false) {
+        placar1.innerHTML = parseInt(placar1.innerHTML) + 1
+    } else if (vencedor === 'O' && empate == false){
+        placar2.innerHTML = parseInt(placar2.innerHTML) + 1
+    }
+}
+                           
 function recomecar() {
 
     a1.innerHTML = ''
@@ -157,6 +161,7 @@ function recomecar() {
     c3.innerHTML = ''
 
     vez = 'O';
+    empate = false
     resul.style.display = 'none'
     resul.innerHTML = ''
     tabuleiro.style.display = 'block'
